d2766afe5ab1c57a0849e679a06d06bc1575bbe6
Nick Hu <hukefeng@gmail.com>
2025-12-03 20:36:39 +0800

    Update CorsConfig.java

--- punchout-gateway/src/main/java/com/waters/punchout/gateway/config/CorsConfig.java
+++ punchout-gateway/src/main/java/com/waters/punchout/gateway/config/CorsConfig.java
@@ -14,9 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                         .allowedHeaders("*")
+                        .allowCredentials(false)
                         .maxAge(3600);
             }
         };
